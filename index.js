3bcd28ae74bf54af1327b726518506d3ac63ff89
rabah babaci <4rabah@gmail.com>
2019-12-03 08:01:42 -0800

    add get car Info

--- index.js
+++ index.js
@@ -86,10 +86,6 @@ function makeSmartPerson(name) {
   };
 }
 
-
-
-
-
 /*
 // ⭐️ Example Test Data ⭐️
 
@@ -145,7 +141,7 @@ function get3rdCar(inventory) {
  * it will return `This is a Lincoln Navigator`.
 */
 function getCarInfoByIndex(inventory, index) {
-  /* code here */
+  return `This is a ${inventory[index].car_make} ${inventory[index].car_model}.`;
 }
 
 /**
@@ -159,8 +155,10 @@ function getCarInfoByIndex(inventory, index) {
  * For example, if getLastCarInfo is invoked passing the inventory inside /data/inventory.js,
  * it will return `This is a Lincoln Town Car`.
 */
-function getLastCarInfo(/* code here */) {
-  /* code here */
+function getLastCarInfo(inventory) {
+  const lastCar = inventory[inventory.length - 1];
+
+  return `This is a ${lastCar.car_make} ${lastCar.car_model}.`
 }
 
 /**
