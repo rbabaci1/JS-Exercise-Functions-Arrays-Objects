c6395e6a30c81af62851eba21f5559fa9689f87a
rabah babaci <4rabah@gmail.com>
2019-12-03 08:39:38 -0800

    get german cars

--- index.js
+++ index.js
@@ -233,7 +233,14 @@ function getModelYears(inventory) {
  * in the same order as they appear in the original inventory.
 */
 function getOlderCars(inventory, max_year) {
-  
+  const oldCars = [];
+
+  for (var i = 0; i < inventory.length; i++) {
+    if (inventory[i].car_year <= max_year) {
+      oldCars.push(inventory[i]);
+    }
+  }
+  return oldCars;
 }
 
 /**
@@ -247,8 +254,16 @@ function getOlderCars(inventory, max_year) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(inventory) {
+  const germanCars = [];
+
+  for (var i = 0; i < inventory.length; i++) {
+    if (inventory[i].car_make === 'Audi' || inventory[i].car_make === 'Mercedes-Benz'||
+     inventory[i].car_make === 'Volkswagen' || inventory[i].car_make === 'BMW' ) {
+        germanCars.push(inventory[i]);
+     }
+  }
+  return germanCars;
 }
 
 /**
@@ -269,9 +284,9 @@ function getGermanCars(/* code here */) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
-const addFive = null; // code here!
-const argTimesTwo = null; // code here!
+const sum = ((a, b) => a + b);
+const addFive = (num => num + 5);
+const argTimesTwo = (num => num * 2);
 
 /**
  * ### Challenge `carMaker`
